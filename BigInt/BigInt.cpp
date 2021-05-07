dbf58154f9e8d7de86c07643826ca389cf6f814f
loc4atnt <dorakid153@gmail.com>
2021-05-07 18:06:23 +0700

    add excess bytes function

--- BigInt/BigInt.cpp
+++ BigInt/BigInt.cpp
@@ -55,8 +55,9 @@ void addByteToBigInt(BigInt* bigInt, byte b) {
 	bigInt->bytes[bigInt->byteCount - 1] = b;
 }
 
-void freeBigInt(BigInt bigI) {
-	free(bigI.bytes);
+void freeBigInt(BigInt* bigI) {
+	free(bigI->bytes);
+	*bigI = DefaultBigInt;
 }
 
 BigInt operator+(BigInt a, BigInt b)
@@ -94,7 +95,7 @@ void convertTwoComplement(BigInt& res)
 	byte v = 1;
 	byte* p = &v;
 	BigInt i = { p,1,0 };
-	res = res + i;
+	//res = res + i;
 }
 
 BigInt oppositeNum(BigInt a)
@@ -107,6 +108,63 @@ BigInt oppositeNum(BigInt a)
 
 BigInt operator-(BigInt a, BigInt b) {
 	BigInt res;
-	res = a + oppositeNum(b);
-	return res;
+	//res = a + oppositeNum(b);
+	//return res;
+
+	return a;
+}
+
+void fillLastByteWithSignExcess(BigInt* i) {
+	if (!i->isHasSign) return;
+	uint8_t lastHighBitIdxInLastByte = 7;
+	while (lastHighBitIdxInLastByte >= 0 && (readBit(i->bytes[i->byteCount - 1], lastHighBitIdxInLastByte) == 0))
+		lastHighBitIdxInLastByte--;
+	for (int idx = lastHighBitIdxInLastByte + 1; idx < 8; idx++) {
+		setBit(&i->bytes[i->byteCount - 1], idx, 1);
+	}
+}
+
+void addSignExcessBytes(BigInt* i, uint8_t amount) {
+	if (i->isHasSign) fillLastByteWithSignExcess(i);
+
+	byte signExcess = i->isHasSign ? 0b11111111 : 0b00000000;
+	for (int idx = 0; idx < amount; idx++) {
+		addByteToBigInt(i, signExcess);
+	}
+}
+
+void removeLastByteFromBigInt(BigInt* bigInt) {
+	if (bigInt->byteCount == 0) return;
+	if (bigInt->byteCount == 1) {
+		freeBigInt(bigInt);
+		return;
+	}
+	bigInt->byteCount--;
+	byte* newMem = (byte*)malloc(bigInt->byteCount);
+	memcpy_s(newMem, bigInt->byteCount, bigInt->bytes, bigInt->byteCount);
+	free(bigInt->bytes);
+	bigInt->bytes = newMem;
+}
+
+void reduceSignExcessFromLastByte(BigInt* bigInt) {
+	if (!bigInt->isHasSign) return;
+	uint8_t signExcessBitLen = 0;
+	while (signExcessBitLen < 8 && readBit(bigInt->bytes[bigInt->byteCount - 1], 7 - signExcessBitLen)==1)
+		signExcessBitLen++;
+	for (int i = 7; i >= 8 - signExcessBitLen; i--) {
+		setBit(&bigInt->bytes[bigInt->byteCount - 1], i, 0);
+	}
+}
+
+void reduceSignExcessBytes(BigInt* i) {
+	byte signExcess = i->isHasSign ? 0b11111111 : 0b00000000;
+	while (i->byteCount > 0 && i->bytes[i->byteCount - 1] == signExcess) {
+		removeLastByteFromBigInt(i);
+	}
+	reduceSignExcessFromLastByte(i);
+}
+
+BigInt operator ~ (BigInt a) {
+	BigInt tmpA = a;
+	return a;
 }
--- BigInt/BigInt.h
+++ BigInt/BigInt.h
@@ -3,11 +3,18 @@
 
 #include "Util.h"
 #include <stdint.h>
+#include <stdio.h>
 
 struct BigInt {
 	byte* bytes;
 	uint16_t byteCount;
 	bool isHasSign;
+
+	BigInt& operator=(BigInt& a) {
+		BigInt hmm = { NULL, 0, false };
+		printf("vadk;fdaf :P");
+		return hmm;
+	}
 };
 
 static BigInt DefaultBigInt = {NULL, 0, false};
@@ -15,11 +22,15 @@ static BigInt DefaultBigInt = {NULL, 0, false};
 BigInt binStrToBigInt(const char* binStr);
 char* bigIntToBinStr(BigInt b);
 void addByteToBigInt(BigInt* bigInt, byte b);
-void freeBigInt(BigInt bigI);
+void freeBigInt(BigInt* bigI);
+void fillLastByteWithSignExcess(BigInt* i);
+void addSignExcessBytes(BigInt* i, uint8_t amount);
+void removeLastByteFromBigInt(BigInt* bigInt);
 
 BigInt operator + (BigInt a, BigInt b);
 void convertTwoComplement(BigInt& a);
 BigInt oppositeNum(BigInt a);
 BigInt operator - (BigInt a, BigInt b);
+BigInt operator ~ (BigInt a);
 
 #endif
